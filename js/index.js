1996864435397cbec6e3f46ac1b6c2f5cef44c73
iwonakulacz <iwonakulacz@gmail.com>
2019-08-23 21:04:24 +0200

    add nav animation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-console.log('hello');
+const nav = document.querySelector('.nav');
+
+window.addEventListener('scroll', ()=> {
+    const scrollTop = window.scrollY;
+    if(scrollTop > 200 && !nav.classList.contains('scroll')){
+        nav.classList.add('scroll');
+    }
+    if(scrollTop < 2){
+        nav.classList.remove('scroll');
+    }
+});
